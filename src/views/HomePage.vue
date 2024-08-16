cb581d449f73e058ef9b4da1f1df8a781c6ba90d
Jaeseok Lee <wotjr4017@gmail.com>
2024-08-17 00:12:05 +0900

    🐛 fix: 배포 수정

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -71,6 +71,12 @@ export default {
             });
         const responseData = await response.json();
 
+        // 오류 응답 처리
+            if (response.status !== 200) {
+              console.error('서버 오류:', responseData.error);
+              return;
+            }
+
          // 응답 데이터 확인
           console.log('응답 데이터:', responseData);
 
